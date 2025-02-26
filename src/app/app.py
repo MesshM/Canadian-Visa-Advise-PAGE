8999f981b223d78929b422cd7382ac5b9b5e9fcd
Manuel Galindo <manuel.agn2005@gmail.com>
2025-02-26 09:29:48 -0500

    correcion ded archivos

--- src/app/app.py
+++ src/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html') 
+    return render_template('index.html') 
 
 if __name__ == "__main__":
     app.run(debug=True, port=5050)
@@ -12,6 +12,6 @@ if __name__ == "__main__":
 import mysql.connector
 
 conexion = mysql.connector.connect(user='root', password='root', 
-                                   host='localhost', 
-                                   database='cva',
-                                   port='3306')
+                                    host='localhost', 
+                                    database='cva',
+                                    port='3306')
