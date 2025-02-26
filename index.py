adf8909846dbe8fa3d1bb721ff1d9994ecad4e49
Manuel Galindo <manuel.agn2005@gmail.com>
2025-02-26 08:36:42 -0500

    database 1.0

--- index.py
+++ index.py
@@ -8,3 +8,10 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True, port=5050)
+
+import mysql.connector
+
+conexion = mysql.connector.connect(user='root', password='root', 
+                                   host='localhost', 
+                                   database='cva',
+                                   port='3306')
