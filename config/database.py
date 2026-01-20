0828ed72becc69d10c7bd56846288a9d87831e49
Manuel Galindo <manuel.agn2005@gmail.com>
2026-01-20 13:40:36 -0500

    Clear database connection credentials
    Updated database connection parameters to empty strings.

--- config/database.py
+++ config/database.py
@@ -4,10 +4,10 @@ from mysql.connector import Error
 def create_connection():
     try:
         connection = mysql.connector.connect(
-            host='cva.ch86isccq37m.us-east-2.rds.amazonaws.com',  # Endpoint de RDS
+            host='',  # Endpoint de RDS
             database='CVA',  # Nombre de tu base de datos
             user='admin',  # Usuario de MySQL en RDS
-            password='root.2025'  # Contraseña de MySQL en RDS
+            password=''  # Contraseña de MySQL en RDS
         )
         if connection.is_connected():
             return connection
